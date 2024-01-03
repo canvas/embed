32023fbc419cf00259a7853aeb140f62a50650f7
Jeremy Bernier <jeremy.s.bernier@gmail.com>
2024-01-03 10:46:25 -0800

    BigNumber handle grid data

--- react/src/components/BigNumber.tsx
+++ react/src/components/BigNumber.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ComponentEmbedElement } from '../types';
-import { isNumeric } from '../util';
 
 export const BigNumber = ({ element }: { element: ComponentEmbedElement }) => {
-    const formattedValue = element.elementType.formattedValues?.find((val) => isNumeric(val));
+    const data = element.elementType.data;
+
+    const currentNumber = data && data[0] && data[0][0] ? data[0][0] : null;
+
+    const formattedValue = currentNumber;
 
     return <> {formattedValue != null && <div className="text-3xl">{formattedValue}</div>}</>;
 };
