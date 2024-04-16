7d9e0a6cbc30a234faaf4439c76f510d9ec8bdb2
Will Pride <wpride@canvasapp.com>
2024-04-16 09:55:24 -0700

    Debounce

--- react/src/components/SearchComponent.tsx
+++ react/src/components/SearchComponent.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { ComponentEmbedElement } from '../types';
 import useCanvasState from '../state/useCanvasState';
+import debounce from 'lodash.debounce';
 
 export const SearchComponent = ({ element, title }: { element: ComponentEmbedElement; title: string }) => {
     const updateFilter = useCanvasState((state) => state.updateFilter);
     const filterVariable = JSON.parse(element.elementType.component.config).filterVariable;
 
+    const debounceUpdateFilter = debounce((value: string) => {
+        updateFilter({ [filterVariable]: [{ value, label: value }] });
+    }, 500);
+
     const [filterValue, setFilterValue] = React.useState<string>('');
     const onFilterChange = (value: string) => {
         setFilterValue(value);
-        updateFilter({ [filterVariable]: [{ value, label: value }] });
+        debounceUpdateFilter(value);
     };
 
     return (
