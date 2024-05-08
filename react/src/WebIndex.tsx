2cfd85a7c00c4cb0ce3f37d1e939b762b56b069b
Will Pride <wpride@canvasapp.com>
2024-05-07 19:13:28 -0700

    Allow null authToken

--- react/src/WebIndex.tsx
+++ react/src/WebIndex.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Canvas } from '.';
 
-export const renderToTag = (elementId: string, canvasId: string, authToken: string, host?: string) => {
+export const renderToTag = (elementId: string, canvasId: string, authToken?: string, host?: string) => {
     const el = document.getElementById(elementId);
     if (!el) return;
     const root = createRoot(el);
